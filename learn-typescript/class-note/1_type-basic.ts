58446f5b559581910554fd0f62bf26981b92f833
cmin0717 <cyun0717@gmail.com>
2023-05-03 22:58:45 +0900

    TS 변수와 함수 타입 정하기

---
+++ learn-typescript/class-note/1_type-basic.ts
@@ -0,0 +1,33 @@
+// js 문자열 선언 방식
+let str = 'hello'
+
+// ts 문자열 선언 방식
+let str1: string = 'hello' // 이 변수는 스트링이 담겨있는 변수다 라는 의미이다.
+
+// ts 숫자형 선언
+let num:number = 10
+
+// ts 배열 선언
+let arr: Array<number> = [1,2,3] // arr라는 변수는 배열이면서 배열의 구성은 number로 하겠다 라는 의미이다.
+let arr1: number[] = [4,5,6]  // 이렇게도 선언할수 있다.
+let str_arr: string[] = ['Capt', 'Thor', 'Hulk'] // 문자형 배열도 선언가능
+// let str_arr1: Array<string> = ['Capt', 'Thor', 'Hulk', 10] // 선언은 문자형 배열로 했는데 숫자형을 넣으면 오류를 발생시킨다.
+let arr2: any[] = [1, 'hi', true] // 각종 타입이 들어가는 배열이라면 특정 타입 말고 any를 넣어준다.
+
+// ts 튜플 선언 / 튜플 : 튜플은 배열의 길이가 고정되고 각 요소의 타입이 지정되어 있는 배열 형식을 의미합니다.
+let address: [string, number] = ['Cpat', 100] // 지정되 배열에 무슨무슨 타입이 들어가나 정의해준다.
+
+// ts 객체 선언
+let obj: object = { // obj라는 변수가 객체타입이다 라는걸 의미한다.
+    name:'Cpat',
+    age : 100
+}
+let info: {name:string, age:number} = { // info라는 변수는 객체이면서 name이라는 문자형과 age라는 숫자형 데이터를 갖는다
+    name:'Cpat',
+    age:100
+}
+// let info: {name:string, age:number} = {} // 만일 이렇게 선언만 한다면 name,age라는 프로퍼티가 있다고 받았는데 막상 없으니 오류를 발생시킨다.
+
+// ts 불린 값
+let show: boolean = true
+
---
+++ learn-typescript/class-note/2_functions.ts
@@ -0,0 +1,44 @@
+// 함수의 파라미터에 타입을 정의하는 방식
+function sum(a:number, b:number){ // 함수식으로 표현
+    return a+b
+}
+const sum1 = (a:number, b:number) => { //화살표 함수로 표현
+    return a+b // number+number을 반환함으로 자동으로 반환 타입이 number로 지정된다.
+}
+sum1(10,20)
+
+
+// 함수의 반환값에 타입을 정의하는 방식
+function add1():number{
+    return 10
+}
+const add = ():number => { // ()뒤에 :타입을 선언하여 해당 타입을 반환하겠다 라는 의미이다.
+    return 10              // 만일 반환 타입을 설정하고 다른 타입이 반환될시 오류가 발생한다.
+}
+add()
+
+
+// 함수의 파라미터와 반환값에 타입 지정
+const tt = (a:string, b:number[]):string => { // a는 문자형, b는 숫자가 들어있는 배열을 받겠다 그리고 문자형으로 리턴하겠다라는 의미이다.
+    return a + b
+}
+
+
+// 함수의 옵셔널 파라미터(선택적 파리미터)
+const log = (a:string, b?:string) => { // a,b라는 파라미터를 받는데 a는 필수로 받아야하지만 ?가 있는 b는 있으면 받고 없으면 안받는다.
+    return a+b
+}
+log('Hulk') // 이렇게 되면 a는 들어갔지만 b는 들어가지 않았기에 b에는 undefine이 들어가게되어서 결과적으로 HulkUndefine이 출력된다.
+log('Cpat', 'Thor')
+
+
+// 함수의 REST 문법이 적용된 매개변수
+const sum3 = (a:number, ...b:number[]):number => { // a변수는 하나를 받고 b변수는 a를 제외한 파라미터를 숫자형 배열에 담아 변수 b에 넣어준다.
+    let cnt:number = 0
+    cnt += a
+    for(let i of b){
+        cnt += i
+    }
+    return cnt
+}
+sum3(1,2,3,4,5) // 1은 a변수에 [2,3,4,5] 는 b변수에 담긴다.
